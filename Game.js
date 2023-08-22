50e0776ed2e3ab8fda3c1bdfe68ae8e53f70be77
Dependra Basnet <126497750+dependrabas@users.noreply.github.com>
2023-08-22 22:44:01 +0530

    Add files via upload

---
+++ Game.js
@@ -0,0 +1,5 @@
+const startGameBtn = document.getElementById('start-game-btn');
+function startGame(){
+    console.log("The game is starting.....")
+}
+startGameBtn.addEventListener('click',startGame);
