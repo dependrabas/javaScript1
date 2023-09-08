0880e666a3fb6850f55687739ae2388db44b7bca
Dependra Basnet <126497750+dependrabas@users.noreply.github.com>
2023-09-08 23:34:18 +0530

    Add files via upload

---
+++ Switch.js
@@ -0,0 +1,9 @@
+var age = prompt("Enter you age:");
+switch (age) {
+    case <13:
+        alert("You must be 13 or older to play");
+        break;
+    case >=13:
+        alert("You are old enough to play");
+        break;
+}
